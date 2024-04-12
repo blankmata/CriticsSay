d24a18cdf2c9d8a79280a95e760333b83cc5f514
David Mata <matad2@jacks.sfasu.edu>
2024-04-12 11:02:17 -0500

    added login/logout changes

--- CriticsSayWeb/Pages/Account/Register.cshtml.cs
+++ CriticsSayWeb/Pages/Account/Register.cshtml.cs
@@ -19,13 +19,39 @@ namespace CriticsSayWeb.Pages.Account
 
         public ActionResult OnPost()
         {
+
             if (ModelState.IsValid)
             {
+                // make sure the email does not exist before registering the user
+                if (EmailDNE(NewPerson.Email))  //EmailDNE = email doesn't exist
+                {
+                    RegisterUser();
+                    return RedirectToPage("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("RegisterError", "This email is already taken. Try a different one");
+                    return Page();
+                }
+
+
                 // Insert Data into Database
                 // 1. Create a datbase connection string
                 //string connString = "Server=(localdb)\\MSSQLLocalDB;Database=CriticSay;Trusted_Connection=true;";  
                 SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnection());
                 // 2. Create an insert command
+      
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        private void RegisterUser()
+        {
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnection()))
+            {
                 string cmdtext = "INSERT INTO Person(FirstName, LastName, Email, Password, LastLoginTime, RoleId)" +
                     "VALUES(@firstName, @lastName, @email, @password, @lastLoginTime, 2)";
                 SqlCommand cmd = new SqlCommand(cmdtext, conn);
@@ -34,17 +60,29 @@ namespace CriticsSayWeb.Pages.Account
                 cmd.Parameters.AddWithValue("@email", NewPerson.Email);
                 cmd.Parameters.AddWithValue("@password", SecurityHelper.GeneratePasswordHash(NewPerson.Password));
                 cmd.Parameters.AddWithValue("@lastLoginTime", DateTime.Now.ToString());
-                // 3. Open the database
-                conn.Open();
-                // 4. Execute the command
+                //cmd.Parameters.AddWithValue("@roleId", NewPerson.RoleId);
+                conn.Open(); 
                 cmd.ExecuteNonQuery();
-                // 5. Close the database
-                conn.Close();
-                return RedirectToPage("Login");
             }
-            else
+        }
+
+        private bool EmailDNE(string email)
+        {
+            using(SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnection()))
             {
-                return Page();
+                string cmdText = "SELECT * FROM Person WHERE Email=@email";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@email", email);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
         }
     }
