97532e1fa3206b15eab1566be1a998d46f6dbab9
David Mata <122194146+blankmata@users.noreply.github.com>
2024-04-18 23:38:12 -0500

    Revert "add/edit/view item to list"

--- CriticsSayWeb/Pages/Account/Login.cshtml.cs
+++ CriticsSayWeb/Pages/Account/Login.cshtml.cs
@@ -1,11 +1,8 @@
 using CriticSayBusiness;
 using CriticsSayWeb.Pages.Model;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
-using System.Security.Claims;
 
 namespace CriticsSayWeb.Pages.Account
 {
@@ -21,99 +18,46 @@ namespace CriticsSayWeb.Pages.Account
         {
             if (ModelState.IsValid)
             {
-                // Check login credentials
-                if (ValidateCredentials())
+                using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnection()))
                 {
-                    return RedirectToPage("Profile");
-                }
-                else
-                {
-                    ModelState.AddModelError("LoginError", "Invalid Credentials, try again.");
-                    return Page();
-                }
-            }
-
-            else
-            {
-                return Page();
-            }
-        }
-
-        private bool ValidateCredentials()
-        {
-            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnection()))
-            {
-                string cmdText = "SELECT Password, PersonId, FirstName, Email, RoleName FROM Person " +
-                    " INNER JOIN [Role] ON Person.RoleId=[Role].RoleId WHERE Email=@email";
-                using (SqlCommand cmd = new SqlCommand(cmdText, conn))
-                {
-                    cmd.Parameters.AddWithValue("@email", LoginUser.Email);
-                    conn.Open();
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.HasRows)
+                    string cmdText = "SELECT Password FROM Person WHERE Email=@email";
+                    using (SqlCommand cmd = new SqlCommand(cmdText, conn))
                     {
-                        reader.Read();
-                        if (!reader.IsDBNull(0))
+                        cmd.Parameters.AddWithValue("@email", LoginUser.Email);
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.HasRows)
                         {
-                            string passwordHash = reader.GetString(0);
-                            if (SecurityHelper.VerifyPasswordHash(LoginUser.Password, passwordHash))
+                            reader.Read();
+                            if (!reader.IsDBNull(0))
                             {
-                                // Get the PersonId and use it to update the Person record
-                                int personId = reader.GetInt32(1);
-                                UpdatePersonLoginTime(personId);
-
-                                // Create a principal
-                                string name = reader.GetString(2);
-                                string RoleName = reader.GetString(4);
-
-                                // create claims
-                                Claim emailClaim = new Claim(ClaimTypes.Email, LoginUser.Email);
-                                Claim nameClaim = new Claim(ClaimTypes.Name, name);
-                                Claim roleClaim = new Claim(ClaimTypes.Role, RoleName);
-
-                                List<Claim> claims = new List<Claim> { emailClaim, nameClaim, roleClaim };
-
-                                // add list of claims to a claimsIdentity
-                                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                                // add the identity to a claimsPrincipal
-                                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
-
-                                // call HttpContext.SigninAsync() method to encrypt the principal
-                                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-
-
-                                return true;
+                                string passwordHash = reader.GetString(0);
+                                if (SecurityHelper.VerifyPasswordHash(LoginUser.Password, passwordHash))
+                                {
+                                    return RedirectToPage("Profile");
+                                }
+                                else
+                                {
+                                    ModelState.AddModelError("LoginError", "Invalid Credentials, try again.");
+                                    return Page();
+                                }
                             }
                             else
                             {
-                                return false;
+                                return Page();
                             }
                         }
                         else
                         {
-                            return false;
+                            ModelState.AddModelError("LoginError", "Invalid Credentials, try again.");
+                            return Page();
                         }
-                    }
-                    else
-                    {
-                        return false;
                     }
                 }
             }
-
-        }
-
-        private void UpdatePersonLoginTime(int personId)
-        {
-            using(SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnection()))
+            else
             {
-                string cmdText = "UPDATE Person SET LastLoginTime=@lastLoginTime WHERE PersonId=@personId";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-                cmd.Parameters.AddWithValue("@lastLoginTime", DateTime.Now);
-                cmd.Parameters.AddWithValue("@personId", personId);
-                conn.Open();
-                cmd.ExecuteNonQuery();
+                return Page();
             }
         }
     }
--- CriticsSayWeb/Program.cs
+++ CriticsSayWeb/Program.cs
@@ -1,15 +1,7 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "MyKitchenCookie";
-        options.LoginPath = "/Account/Login";
-    });
 
 var app = builder.Build();
 
